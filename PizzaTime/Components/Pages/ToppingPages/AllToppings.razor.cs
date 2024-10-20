c23cd9a5854312b885e37ead27c442807cf7b23b
NikodemWspanialy <nikodemwspanialy@gmail.com>
2024-10-20 17:24:12 +0200

    Add topping list  and segregate images

---
+++ PizzaTime/Components/Pages/ToppingPages/AllToppings.razor.cs
@@ -0,0 +1,31 @@
+﻿using Architecture.Toppings.Query.GetAllToppingsQuery;
+using Domain.Models;
+
+namespace PizzaTime.Components.Pages.ToppingPages
+{
+	public partial class AllToppings
+	{
+		private List<Topping> toppings = [];
+		private bool IsProcessing = true;
+
+		protected override async Task OnAfterRenderAsync(bool firstRender)
+		{
+			if (firstRender)
+			{
+				await LoadToppings();
+			}
+		}
+
+		private async Task LoadToppings()
+		{
+			await Task.Delay(2000);
+			var result = await mediator.Send(new GetToppingsQuery());
+			if (result is not null)
+			{
+				toppings = result.ToList();
+				IsProcessing = false;
+				StateHasChanged();
+			}
+		}
+	}
+}
