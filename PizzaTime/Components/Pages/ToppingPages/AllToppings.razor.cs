9e4be47405375c657d78100ead8a48ca0695e77e
NikodemWspanialy <nikodemwspanialy@gmail.com>
2024-10-23 20:11:02 +0200

    add filter to all toppping page

--- PizzaTime/Components/Pages/ToppingPages/AllToppings.razor.cs
+++ PizzaTime/Components/Pages/ToppingPages/AllToppings.razor.cs
@@ -1,11 +1,14 @@
 ﻿using Architecture.Toppings.Query.GetAllToppingsQuery;
 using Domain.Models;
+using Microsoft.AspNetCore.Components;
+using System.Linq;
 
 namespace PizzaTime.Components.Pages.ToppingPages
 {
 	public partial class AllToppings
 	{
 		private List<Topping> toppings = [];
+		private List<Topping> allToppings = [];
 		private bool IsProcessing = true;
 
 		protected override async Task OnAfterRenderAsync(bool firstRender)
@@ -22,10 +25,30 @@ namespace PizzaTime.Components.Pages.ToppingPages
 			var result = await mediator.Send(new GetToppingsQuery());
 			if (result is not null)
 			{
-				toppings = result.ToList();
+				allToppings = result.ToList();
+				toppings = allToppings;
 				IsProcessing = false;
 				StateHasChanged();
 			}
+		}
+
+		private void filterToppings(ChangeEventArgs e)
+		{
+			if (e is not null)
+			{
+				string filter = e.Value?.ToString();
+
+				if (!string.IsNullOrEmpty(filter))
+				{
+					toppings = allToppings.Where(t => t.Name.Contains(filter)).ToList();
+				}
+				else
+				{
+					toppings = allToppings;
+				}
+			}
+
+			StateHasChanged();
 		}
 	}
 }
